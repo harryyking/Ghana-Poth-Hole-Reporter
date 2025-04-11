ed414fc40e11568e100a3c10e1acf7cb32c2ce2e
harryyking <arthurharry06@gmail.com>
2025-04-11 16:31:24 +0000

    add page

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,9 +1,10 @@
+import { AuthFormExample } from '@/components/auth/auth-form-example'
 import React from 'react'
 
 const page = () => {
   return (
-    <div className='flex justify-center items-center p-2'>
-        Auth
+    <div className='flex justify-center items-center p-2 min-h-screen'>
+        <AuthFormExample/>
     </div>
   )
 }
